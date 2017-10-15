a5f4845f2ca6430a4629df94cbfad3094fd20715
Michael van de Waeter <mvdwaeter@gmail.com>
2017-10-15 15:17:38 +0200

    Make use of ii-django-package-settings

---
+++ ii_drf_pagination/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'ideallical DRF pagination'
+__version__ = '0.2'
+__author__ = 'ideallical'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2017 ideallical'
+
+# Version synonym
+VERSION = __version__
--- setup.py
+++ setup.py
@@ -1,17 +1,60 @@
+import os
+import re
+from io import open
+
 from setuptools import setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+
+    return {package: filepaths}
+
+
+version = get_version('ii_drf_pagination')
 
 setup(
     name='ii_drf_pagination',
-    version='0.1',
+    version=version,
     description='ideallical django-restframework pagination',
     url='https://github.com/ideallical/ii-drf-pagination',
     download_url=('https://github.com/ideallical/ii-drf-pagination/archive/'
-                  '0.1.tar.gz'),
+                  '{}.tar.gz'.format(version)),
     author='ideallical',
     author_email='info@ideallical.com',
-    keywords=['testing', 'drf', 'pagination'],
+    keywords=['drf', 'pagination'],
     license='BSD',
-    install_requires=[],
+    install_requires=[
+        'ii-django-package-settings>=0.1',
+    ],
     packages=['ii_drf_pagination'],
     zip_safe=False
 )
